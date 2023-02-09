98ec3895a662ed0b3b3d3fc1ac54e074c851c44a
JiSuMun <mjs3827@naver.com>
2023-02-10 03:01:31 +0900

    add 230209

--- BOJ/1453.py
+++ BOJ/1453.py
@@ -1,20 +1,40 @@
 """
 피시방에는 1~100번까지 컴퓨터 있다.
-들어오면서 번호를 말한다. 그 자리 사람 없으면 그 손님 앉아서 컴퓨터하고 아니면 거절당한다.
-
+들어오면서 번호를 말한다. 그 자리 사람 없으면 그 손님 앉아서 컴퓨터, 아니면 거절당한다.
+거절당하는 사람의 수를 출력하는 프로그램
+자리는 맨 처음에 모두 비어있고, 어떤 사람이 자리에 앉으면 자리를 비우는 일은 없다
 """
 # 과거 풀이
-#
-
+# 48ms
+import sys
+input = sys.stdin.readline
+N = int(input())
+PC = list(map(int, input().split()))
+cnt = 0
+s = []
+for i in range(N):
+    if PC[i] in s:
+        cnt += 1
+    else:
+        s.append(PC[i])
+print(cnt)
 
 """
-
+거절당하는 사람 수 cnt라는 변수 선언
+손님이 앉고 싶어하는 자리 리스트 PC를 입력받음
+손님수만큼 반복문을 돌리면서 빈 리스트 s에 PC리스트안의 자리가 있으면 cnt += 1, 없으면 리스트에 추가
 """
 
 # 현재 풀이
-#
-
+# 36ms
+import sys
+input = sys.stdin.readline
+N = int(input())
+PC = list(map(int, input().split()))
+s = len(list(set(PC)))
+print(N-s)
 
 """
-
+앉고 싶어하는 자리 리스트를 set으로 중복을 없앤 것을 리스트로 만들어 len으로 개수를 센다
+거절당하는 사람 수 = 손님 수 - 중복없앤 자리리스트 개수
 """
--- BOJ/1526.py
+++ BOJ/1526.py
@@ -1,9 +1,45 @@
+"""
+금민수는 어떤 수가 4와 7로만 이루어진 수
+N이 주어졌을 때, N보다 작거나 같은 금민수 중 가장 큰 것을 출력하는 프로그램
+"""
 # 과거 풀이
-#
-
-# 현재 풀이
-#
+# 112ms
+import sys
+input = sys.stdin.readline
+N = int(input())
+while 1:
+    flag = True
+    for i in str(N):
+        if i != '4'  and i != '7':
+            flag = False
+            N -= 1
+    if flag:
+        print(N)
+        break
 
 """
+N을 입력받고 while문을 사용하여 반복문을 돌리면서 가장 큰 것을 구하면 출력하며 종료하는 조건을 주었다.
+입력받은 정수를 문자열로 바꾸고 그 문자열의 원소를 i로 for문의 반복문을 돌린다.
+문자열의 원소가 4와 7이 아니면 금민수가 아니므로 False로 바꾸고 숫자를 하나씩 줄인다.
+가장 큰 수부터 반복을 시작하기 때문에 작은 수부터 시작하는 것보다 반복 횟수를 줄일 수 있다.
+"""
 
+# 현재 풀이
+# 44ms
+def solve(n):
+    res = n
+    if n * 10 + 4 <= N:
+        res = max(res, solve(n * 10 + 4))
+    if n * 10 + 7 <= N:
+        res = max(res, solve(n * 10 + 7))
+    return res
+
+N = int(input())
+print(solve(0))
+
+"""
+함수 정의하여 사용
+n=0부터 시작해야 4, 7도 가능
+def문 안에서도 def로 반복해서 큰값을 res에 저장하여 리턴함
+다시 풀어보니 시간복잡도 부분에서 상당히 개선되었다.
 """
